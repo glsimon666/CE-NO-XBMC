CoreELEC/xbmc

Revert "Remove amlogic platform"
This reverts commit 750b8c4b2408938ff0d0b26e732de4e6c8c692b0.

xbmc/windowing/amlogic/WinSystemAmlogic.h
@@ -1,0 +1,56 @@
+/*
+ *  Copyright (C) 2005-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "platform/linux/input/LibInputHandler.h"
+#include "rendering/gles/RenderSystemGLES.h"
+#include "threads/CriticalSection.h"
+#include "windowing/WinSystem.h"
+#include "threads/SystemClock.h"
+#include "EGL/egl.h"
+
+class IDispResource;
+
+class CWinSystemAmlogic : public CWinSystemBase
+{
+public:
+  CWinSystemAmlogic();
+  virtual ~CWinSystemAmlogic();
+
+  bool InitWindowSystem() override;
+  bool DestroyWindowSystem() override;
+
+  bool CreateNewWindow(const std::string& name,
+                       bool fullScreen,
+                       RESOLUTION_INFO& res) override;
+
+  bool DestroyWindow() override;
+  void UpdateResolutions() override;
+
+  bool Hide() override;
+  bool Show(bool show = true) override;
+  virtual void Register(IDispResource *resource);
+  virtual void Unregister(IDispResource *resource);
+protected:
+  std::string m_framebuffer_name;
+  EGLDisplay m_nativeDisplay;
+  EGLNativeWindowType m_nativeWindow;
+
+  int m_displayWidth;
+  int m_displayHeight;
+
+  RENDER_STEREO_MODE m_stereo_mode;
+
+  bool m_delayDispReset;
+  XbmcThreads::EndTime m_dispResetTimer;
+
+  CCriticalSection m_resourceSection;
+  std::vector<IDispResource*> m_resources;
+  std::unique_ptr<CLibInputHandler> m_libinput;
+};

glsimon666/CE-NO-XBMC

xbmc/windowing/amlogic/WinSystemAmlogic.h
@@ -1,0 +1,56 @@
+/*
+ *  Copyright (C) 2005-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
+ */
+
+#pragma once
+
+#include "platform/linux/input/LibInputHandler.h"
+#include "rendering/gles/RenderSystemGLES.h"
+#include "threads/CriticalSection.h"
+#include "windowing/WinSystem.h"
+#include "threads/SystemClock.h"
+#include "EGL/egl.h"
+
+class IDispResource;
+
+class CWinSystemAmlogic : public CWinSystemBase
+{
+public:
+  CWinSystemAmlogic();
+  virtual ~CWinSystemAmlogic();
+
+  bool InitWindowSystem() override;
+  bool DestroyWindowSystem() override;
+
+  bool CreateNewWindow(const std::string& name,
+                       bool fullScreen,
+                       RESOLUTION_INFO& res) override;
+
+  bool DestroyWindow() override;
+  void UpdateResolutions() override;
+
+  bool Hide() override;
+  bool Show(bool show = true) override;
+  virtual void Register(IDispResource *resource);
+  virtual void Unregister(IDispResource *resource);
+protected:
+  std::string m_framebuffer_name;
+  EGLDisplay m_nativeDisplay;
+  EGLNativeWindowType m_nativeWindow;
+
+  int m_displayWidth;
+  int m_displayHeight;
+
+  RenderStereoMode m_stereo_mode;
+
+  bool m_delayDispReset;
+  XbmcThreads::EndTime<> m_dispResetTimer;
+
+  CCriticalSection m_resourceSection;
+  std::vector<IDispResource*> m_resources;
+  std::unique_ptr<CLibInputHandler> m_libinput;
+};